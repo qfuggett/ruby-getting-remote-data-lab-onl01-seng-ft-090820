cf3d24fccca26dd2e574ae6055c48a0580e7b253
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-29 19:12:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,8 +12,8 @@ class GetRequester
   
   def get_response_body
     @uri = URI.parse(@url)
-    response = Net::HTTP.get_response(@uri)
-    response.body
+    @response = Net::HTTP.get_response(@uri)
+    @response.body
     
   end
   
